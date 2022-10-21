eeaeb18e6269f872250d7bf3e3cdd2acf5ecfc17
Dave Harms <daveharmswebdev@gmail.com>
2022-10-21 11:31:15 -0500

    initial commit

---
+++ src/main/java/com/appsdeveloperblog/photoapp/photoappapiaccount/PhotoAppApiAccountApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.photoappapiaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiAccountApplication.class, args);
+	}
+
+}
